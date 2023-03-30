4c8826b7cb1170892f92d2f136f03989926b7ee5
Emily Qi <eqi@umd.edu>
2023-03-29 21:32:49 -0400

    Created favorite course and contact form pages, referencing the base parent page.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,11 +10,26 @@ def hello_world():  # put application's code here
 @app.route('/hello')
 def hello():
     return render_template('hello.html')
-if __name__ == '__main__':
-    app.run()
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
+
+
+@app.route('/favorite-course')
+def favorite_course():
+    print('Subject: ' + request.args.get('subject'))
+    print('Course Number: ' + request.args.get('course_number'))
+
+    return render_template('favorite-course.html')
+
+
+@app.route('/contact', methods=['GET', 'POST'])
+def contact():
+    if request.method == 'POST':
+        return render_template('contact.html', form_submitted=True)
+    else:
+        return render_template('contact.html')
 if __name__ == '__main__':
     app.run()
